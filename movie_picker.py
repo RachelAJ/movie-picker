b10e11e3ff8c944a56a8f399331d55c62fd2f102
Rachel Johnson <rjohns0783@gmail.com>
2020-11-05 13:35:00 -0500

    Took out IMDb databas and added pre-poulated lists

--- movie_picker.py
+++ movie_picker.py
@@ -1,61 +1,25 @@
-import imdb
 import random
-from bs4 import BeautifulSoup
 
-moviesDB = imdb.IMDb()
+horror_movies = ["Halloween", "Friday the 13th", "The Shining"]
+comedy_movies = ["Monty Python and the Holy Grail", "School of Rock", "The Blues Brothers"]
+fantasy_movies = ["Star Wars - A New Hope", "Lord of the Rings", "Harry Potter and the Sorcerer's Stone"]
 
-# print(dir(moviesDB))
+choose_genre = input("Pick one of the following genres - Horror, Comedy, or Fantasy: ")
 
-movies = moviesDB.search_movie("Halloween")
-
-genres = ["Horror", "Comedy", "Drama"]
-
-choose_genre = input("Pick one of the following genres - Horror, Comedy, or Drama: ")
-
-for horror in genres:
+def genre():
     if choose_genre == "Horror":
         print(choose_genre + ", " + "oooo spooky!")
-        break
-
-for comedy in genres:
-    if choose_genre == "Comedy":
+        print("You should watch", random.choice(horror_movies))
+    elif choose_genre == "Comedy":
         print("Ahh yes, " + choose_genre + ", " + "get ready for a laugh!")
-        break
+        print("You should watch", random.choice(comedy_movies))
+    elif choose_genre == "Fantasy":
+        print(choose_genre + ", " + "time to travel to a galaxy far, far away, the Shire, or Hogwarts!")
+        print("You should watch", random.choice(fantasy_movies))
+    else:
+        try:
+            raise ValueError
+        except ValueError:
+            print("Please choose from the 3 genres listed.")
+genre()
 
-for drama in genres:    
-    if choose_genre == "Drama":
-        print(choose_genre + ", " + "great choice! Get the tissues ready!")
-        break
-
-# if choose_genre == "Horror":
-#     print(choose_genre + ", " + "oooo spooky!")
-# elif choose_genre == "Comedy":
-#     print("Ahh yes, " + choose_genre + ", " + "get ready for a laugh!")
-# elif choose_genre == "Drama":
-#     print(choose_genre + ", " + "great choice! Get the tissues ready!")
-# else:
-#     print("Please choose from the 3 genres listed.")
-
-
-id = movies[0].getID()
-movie = moviesDB.get_movie(id)
-
-title = movie['title']
-year = movie['year']
-rating = movie['rating']
-genre = movie['genre']
-directors = movie['directors']
-cast = movie['cast']
-
-print('Movie Info:')
-print(f'{title} - {year}')
-print(f'Rating: {rating}')
-
-genre = ', '.join(map(str, genre))
-print(f'Genre: {genre}')
-
-direcStr = ''.join(map(str, directors))
-print(f'Directors: {direcStr}')
-
-actors = ', '.join(map(str, cast))
-print(f'Cast: {actors}')
